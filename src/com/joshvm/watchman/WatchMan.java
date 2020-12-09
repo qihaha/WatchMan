5a7ddf0307f5467612312cfff1972fb323cdadac
yaoqi.li <yaoqi.li@bangcle.com>
2020-12-09 20:09:08 +0800

    调通m680到传感器

--- src/com/joshvm/watchman/WatchMan.java
+++ src/com/joshvm/watchman/WatchMan.java
@@ -8,6 +8,7 @@ import javax.microedition.io.StreamConnection;
 import com.joshvm.watchman.mqtt.Publisher;
 import com.joshvm.watchman.mqtt.Subscriber;
 import com.joshvm.watchman.uart.UartUtils;
+import com.joshvm.watchman.uart.UartUtils;
 
 /**
  * WatchMan
@@ -19,8 +20,9 @@ import com.joshvm.watchman.uart.UartUtils;
  */
 public class WatchMan {
 
-	private static final String COM = "COM1";
+	private static final String COM = "COM0";
 	private static final int BAUDRATE = 9600;
+	private static final byte[] DOCE_BYTE = new byte[] { 1, 3, 1, 2, 0, 1, 36, 54 }; //"0103010200012436"
 
 	public static final String SERVER_URI = "tcp://39.98.64.167:61613";
 	public static final String CLIENT_ID = Utils.getImei();
@@ -28,40 +30,44 @@ public class WatchMan {
 	public static final String PASSWORD = "password";
 	public static final String TOPIC = "testbroker";
 
-	private static final String CODE = "0103010200012436";
 
 	public static void main(String[] args) {
 
 		System.out.println("Start WatchMan ...");
-		final UartUtils uartUtils = new UartUtils(COM, BAUDRATE);
+		String message = new UartUtils(WatchMan.COM,WatchMan.BAUDRATE,WatchMan.DOCE_BYTE).read();
+		Publisher publisher = new Publisher(WatchMan.SERVER_URI,WatchMan.CLIENT_ID,WatchMan.USER_NAME,WatchMan.PASSWORD);
+		publisher.push(WatchMan.TOPIC, message);
+		System.out.println("getmsg:"+message);
+//		final UartUtils uartUtils = new UartUtils(COM, BAUDRATE);
 		// Publisher
 		 
 //		 Subscriber subscriber = new Subscriber(SERVER_URI,CLIENT_ID,USER_NAME,PASSWORD,TOPIC,null);
 		// 启动子线程，每1s发送数据
-		new Thread(new Runnable() {
-			public void run() {
-				while (true) {
-					try {
-						uartUtils.writeData(CODE);
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}).start();
+//		new Thread(new Runnable() {
+//			public void run() {
+//				while (true) {
+//					try {
+//						uartUtils.writeData(DOCE_BYTE);
+//						System.out.println("sender start");
+//						Thread.sleep(500);
+//					} catch (InterruptedException e) {
+//						e.printStackTrace();
+//					}
+//				}
+//			}
+//		}).start();
 		
-		while(true){
-		try {
-			Thread.sleep(1000);
-			System.out.println("receive start");
-			String message = uartUtils.readData();
-			// push to mqtt.server
-//			 publisher.push(TOPIC, "1222");
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		}
+//		while(true){
+//		try {
+//			Thread.sleep(1000);
+//			System.out.println("receive start");
+//			String message = uartUtils.readData();
+//			// push to mqtt.server
+////			 publisher.push(TOPIC, "1222");
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//		}
 
 
 	}
--- src/com/joshvm/watchman/uart/UartUtils.java
+++ src/com/joshvm/watchman/uart/UartUtils.java
@@ -3,102 +3,85 @@ package com.joshvm.watchman.uart;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-
 import javax.microedition.io.Connector;
 import javax.microedition.io.StreamConnection;
 
-import com.joshvm.watchman.WatchMan;
-import com.joshvm.watchman.mqtt.Publisher;
-
-/**
- * UartUtils
- * <p>
- * 实现串口通信。
- * 
- * @author Administrator
- *
- */
 public class UartUtils {
-	
+	private String com = "COM0";
+	private int baudrate = 9600;
+	private byte[] cmd = new byte[] { 1, 3, 1, 2, 0, 1, 36, 54 };
+	private static StreamConnection streamConnection;
+	private static InputStream inputStream;
+	private static OutputStream outputStream;
 
-	private StreamConnection streamConnection;
-	public InputStream inputStream;
-	public OutputStream outputStream;
+	public UartUtils(String com, int baudrate, byte[] cmd) {
+		this.com = com;
+		this.baudrate = baudrate;
+		this.cmd = cmd;
+	}
 
-	public UartUtils(String com, int baudrate) {
-		// 建立连接
-		String host = "comm:" + com + ";baudrate=" + baudrate;
+	public String read() {
+		final StringBuffer receiveData = new StringBuffer();
 		try {
+			String host = "comm:" + com + ";baudrate=" + baudrate;
 			streamConnection = (StreamConnection) Connector.open(host);
 			inputStream = streamConnection.openInputStream();
 			outputStream = streamConnection.openOutputStream();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+			outputStream.write(cmd, 0, 8);
+//			System.out.println("send:" + byte2hex(cmd));
+			// 启动子线程接收数据，避免read阻塞
+			new Thread(new Runnable() {
+				public void run() {
+					try {
+						byte[] buffer = new byte[256];
+						int len = inputStream.read(buffer);
+						receiveData.append(byte2hex(buffer, len));
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
 
-	public String readData() {
-		String message = "";
-		try {
-			int len = 0;
-			byte[] buffer = new byte[7];
-			while ((len = inputStream.read(buffer)) != -1) {
-				message = bytesToHexString(buffer);
-				System.out.println("get:"+message);
-				Publisher publisher = new Publisher(WatchMan.SERVER_URI,WatchMan.CLIENT_ID,WatchMan.USER_NAME,WatchMan.PASSWORD);
-				publisher.push(WatchMan.TOPIC, message);
+			}).start();
+			Thread.sleep(3000);
+//			System.out.println("readmsg: " + receiveData);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				closeStream();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
-		return message;
+		return receiveData.toString();
 	}
 
-	public boolean writeData(String message) {
-		try {
-			outputStream.write(hexToByteArray(message));
-			return true;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
+	private static void closeStream() throws IOException {
+		if (inputStream != null) {
+			inputStream.close();
 		}
-	}
-	
-	
-	public static byte[] hexToByteArray(String inHex) {
-	    int hexlen = inHex.length();
-	    byte[] result;
-	    if (hexlen % 2 == 1) {
-	      // 奇数
-	      hexlen++;
-	      result = new byte[(hexlen / 2)];
-	      inHex = "0" + inHex;
-	    } else {
-	      // 偶数
-	      result = new byte[(hexlen / 2)];
-	    }
-	    int j = 0;
-	    for (int i = 0; i < hexlen; i += 2) {
-	      result[j] = hexToByte(inHex.substring(i, i + 2));
-	      j++;
-	    }
-	    return result;
-	  }
-	
-	  private static byte hexToByte(String inHex) {
-		    return (byte) Integer.parseInt(inHex, 16);
-		  }
+		if (outputStream != null) {
+			outputStream.close();
+		}
+		if (streamConnection != null) {
+			streamConnection.close();
+		}
 
-	  public static String bytesToHexString(byte[] bArray) {
-		    StringBuffer sb = new StringBuffer(bArray.length);
-		    String sTemp;
-		    for (int i = 0; i < bArray.length; i++) {
-		      sTemp = Integer.toHexString(0xFF & bArray[i]);
-		      if (sTemp.length() < 2){
-		        sb.append(0);
-		      }
-		      sb.append(sTemp.toUpperCase());
-		    }
-		    return sb.toString();
-		  }
+	}
+
+	private static String byte2hex(byte[] data) {
+		return byte2hex(data, data.length);
+	}
+
+	private static String byte2hex(byte[] data, int len) {
+		StringBuffer stringBuffer = new StringBuffer();
+		for (int i = 0; i < len; ++i) {
+			if (data[i] < 16 && data[i] > -1) {
+				stringBuffer.append("0" + Integer.toHexString(data[i] & 255) + " ");
+			} else {
+				stringBuffer.append(Integer.toHexString(data[i] & 255) + " ");
+			}
+		}
+		return stringBuffer.toString();
+	}
 }
